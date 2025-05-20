e17b54af537a351b3226f2712d779eea2bb52417
Andrii2221 <113189637+Andrii2221@users.noreply.github.com>
2025-05-20 18:25:46 +0200

    final update

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import requests
 import telebot
-
-bot = telebot.TeleBot(7519194105:AAEuScnXthxWVgO8b9TRScwyC_wVP44Ktzc)
+import json
+bot = telebot.TeleBot('7519194105:AAEuScnXthxWVgO8b9TRScwyC_wVP44Ktzc')
 API = 'token'
 
 
@@ -14,6 +15,17 @@ def start(message):
 @bot.message_handler(content_types=['text'])
 def get_weather(message):
     city = message.text.strip().lower()
+    res = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API}&units=metric')
+    if res.status_code == 200:
+       data = json.loads(res.text)
+       temp = data['main']['temp']
+       bot.reply_to(message, f'Pogoda jest taka: {temp}')
 
-bot.polling(none_stop=True)
+       image = 'kartinka.1' if temp > 5.0 else 'kartinka.2'
+       file = open('./' + image, 'rb')
+       bot.send_photo(message.chat.id, file)
+    else :
+         bot.reply_to(message, f'Nazwa miasta wpisana nie prawidłowo')
+
+    bot.polling(none_stop=True)
 
